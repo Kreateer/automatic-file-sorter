80523969c7184fb4fe8731748e6acc2c663eb920
Matija <kreateer@protonmail.com>
2020-10-01 14:47:08 +0200

    Update main files

--- fmgui.py
+++ fmgui.py
@@ -4,25 +4,7 @@ import PySimpleGUI as sg
 
 source_folder = sg.popup_get_folder('Choose source location:', default_path='')
 destination_folder = sg.popup_get_folder('Choose destination folder:', default_path='')
-#
-# if not img_folder:
-#     sg.popup_cancel('Cancelling')
-#     raise SystemExit()
-#
-# img_types = (".png")
-#
-# file_list = os.listdir(img_folder)
-#
-# fnames = [f for f in file_list if os.path.isfile(
-#     os.path.join(img_folder, f)) and f.lower().endswith(img_types)]
-#
-# num_files = len(fnames)
-#
-# if num_files == 0:
-#     sg.popup('No files in folder!')
-#     raise SystemExit()
-#
-# del file_list
+
 
 folders = {source_folder: destination_folder}
 file_type = []
@@ -57,13 +39,10 @@ def filemover():
         else:
             for file in os.listdir(*source):
                 file_ending = str(file_type)[2:-2]
-                # "".join(repr(f) for f in file_type)
-                # str(file_type)[1:-1]
                 if file.endswith(file_ending):
                     result = shutil.move(str(*source) + "/" + file, str(*destination) + "/" + file)
                     is_file_in_curr_dir = os.path.isfile(file)
                     current_dir = os.listdir(*destination)
-                    # print(f"File(s) moved to '{str(*destination)}'")
                     if is_file_in_curr_dir not in current_dir:
                         file_type.pop()
         return sg.PopupOK(f"File transfer successful!\nFile(s) moved to '{str(*destination)}'")
