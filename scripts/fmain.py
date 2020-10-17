97b80deea62e89b12240460a64fd6b3c77580562
Emerald <44278515+EmeraldEntities@users.noreply.github.com>
2020-10-17 14:39:47 -0400

    added functionality for video and audio files
    The script will now search and sort video and audio files.
    
    Also added some other common formats for the other file types.

--- scripts/fmain.py
+++ scripts/fmain.py
@@ -3,15 +3,23 @@ import shutil
 import PySimpleGUI as sg
 
 source_folder = sg.popup_get_folder('Choose source location:', default_path='')
-destination_folder = sg.popup_get_folder('Choose destination folder:', default_path='')
+destination_folder = sg.popup_get_folder(
+    'Choose destination folder:', default_path='')
 
 file_type = []
 mode_list = []
 sort_list = []
 
-image_list = ['.png', '.jpg', '.jpeg', '.gif','.bmp','.tif','.psd']
-archive_list = ['.zip', '.rar', '.7z','.deb','.tar','.pkg','.tar.gz,'.rpm','.z']
-textf_list = ['.txt', '.md', '.pdf', '.doc', '.docx', '.xls', '.xlsx','.csv']
+image_list = ['.png', '.jpg', '.jpeg', '.jfif',
+              '.heic', '.gif', '.bmp', '.tif', '.psd']
+archive_list = ['.zip', '.rar', '.7z', '.deb',
+                '.tar', '.pkg', '.tar.gz', '.gz', '.rpm', '.z']
+textf_list = ['.txt', '.md', '.pdf', '.doc',
+              '.docx', '.xls', '.xlsx', '.csv', '.rtf', '.tex']
+video_list = ['.mp4', '.m4v', '.mov', '.wmv',
+              '.flv', '.avi', '.mpeg', '.mpg', '.mpe']
+audio_list = ['.m4a', '.mp3', '.wav', '.flac',
+              '.wma', '.aac', '.aiff', '.aif', '.aifc']
 
 
 def get_path(src_or_dst):
@@ -36,17 +44,27 @@ class fmGUI:
                  "Overwrite files",
                  tooltip="Incoming files will replace files of the same names in the destination",
                  key="OVERWRITE"
-             )],
+            )],
             [sg.Frame(layout=[
                 [sg.Text("Sort by Type")],
                 [sg.Radio("Enabled", "RADIO1", default=False, key='SBYTE', enable_events=True),
                  sg.Radio("Disabled", "RADIO1", default=True, key='SBYTD', enable_events=True)]
-                 ], title='Sorting Options', title_color='red', relief=sg.RELIEF_SUNKEN)],
+            ], title='Sorting Options', title_color='red', relief=sg.RELIEF_SUNKEN)],
             [sg.Text("Choose filetype:")],
-            [sg.Combo(["Archive ('.zip', '.rar'...)", "Image ('.png', '.jpg'...)", "Text ('.txt', '.docx'...)"], key='FILETYPE', enable_events=True)],
+            [sg.Combo(  # Options for type of file
+                [
+                    "Archive ('.zip', '.rar'...)",
+                    "Image ('.png', '.jpg'...)",
+                    "Text ('.txt', '.docx'...)",
+                    "Video ('.mp4', '.mov'...)",
+                    "Audio ('.mp3', '.wav'...)"
+                ],
+                key='FILETYPE', enable_events=True
+            )],
             [sg.Ok(), sg.Cancel()]]
 
-        window = sg.Window('Choose filetype to move', layout, default_element_size=(40, 1))
+        window = sg.Window('Choose filetype to move', layout,
+                           default_element_size=(40, 1))
 
         while True:
             event, values = window.read()
@@ -60,11 +78,14 @@ class fmGUI:
                     if len(sort_list) == 1:
                         for value in sort_list:
                             if value == 'Sort by Type':
-                                run_fmover.filemover(values['OPERATION'], value, values['OVERWRITE'])
+                                run_fmover.filemover(
+                                    values['OPERATION'], value, values['OVERWRITE'])
                             else:
-                                run_fmover.filemover(values['OPERATION'], None, values['OVERWRITE'])
+                                run_fmover.filemover(
+                                    values['OPERATION'], None, values['OVERWRITE'])
                     else:
-                        run_fmover.filemover(values['OPERATION'], None, values['OVERWRITE'])
+                        run_fmover.filemover(
+                            values['OPERATION'], None, values['OVERWRITE'])
 
                 else:
                     run_fmover = FileMover()
@@ -72,11 +93,14 @@ class fmGUI:
                     if len(sort_list) == 1:
                         for value in sort_list:
                             if value == 'Sort by Type':
-                                run_fmover.filemover(values['OPERATION'], value, values['OVERWRITE'])
+                                run_fmover.filemover(
+                                    values['OPERATION'], value, values['OVERWRITE'])
                             else:
-                                run_fmover.filemover(values['OPERATION'], None, values['OVERWRITE'])
+                                run_fmover.filemover(
+                                    values['OPERATION'], None, values['OVERWRITE'])
                     else:
-                        run_fmover.filemover(values['OPERATION'], None, values['OVERWRITE'])
+                        run_fmover.filemover(
+                            values['OPERATION'], None, values['OVERWRITE'])
 
             elif event in 'SBYTE':
                 if values['SBYTE'] is True:
@@ -94,23 +118,39 @@ class fmGUI:
 
         window.close()
 
+
 def translate_filetype():
     for value in file_type:
         if value.startswith("Archive"):
             file_type.clear()
             for arch in archive_list:
-              file_type.append(arch)
+                file_type.append(arch)
             return str(file_type)
+
         elif value.startswith("Image"):
             file_type.clear()
             for img in image_list:
-              file_type.append(img)
+                file_type.append(img)
             return str(file_type)
+
         elif value.startswith("Text"):
             file_type.clear()
             for txt in textf_list:
-              file_type.append(txt)
+                file_type.append(txt)
             return str(file_type)
+
+        elif value.startswith("Video"):
+            file_type.clear()
+            for vid in video_list:
+                file_type.append(vid)
+            return str(file_type)
+
+        elif value.startswith("Audio"):
+            file_type.clear()
+            for aud in audio_list:
+                file_type.append(aud)
+            return str(file_type)
+
         else:
             pass
 
@@ -146,38 +186,48 @@ class FileMover():
             elif sortby is None:
                 for file in os.listdir(get_path('src')):
                     #file_ending = get_file_type()
-                    is_file_in_curr_dir = os.path.isfile(get_path('dst') + "/" + file)
+                    is_file_in_curr_dir = os.path.isfile(
+                        get_path('dst') + "/" + file)
                     for value in file_type:
                         if file.endswith(value):
                             result = None
                             if is_file_in_curr_dir is False or overwrite:
                                 if operation == "Copy":
-                                    result = shutil.copy(get_path('src') + "/" + file, get_path('dst') + "/" + file)
+                                    result = shutil.copy(
+                                        get_path('src') + "/" + file, get_path('dst') + "/" + file)
                                 else:
-                                    result = shutil.move(get_path('src') + "/" + file, get_path('dst') + "/" + file)
-                        #if file not in current_dir:
+                                    result = shutil.move(
+                                        get_path('src') + "/" + file, get_path('dst') + "/" + file)
+                        # if file not in current_dir:
                         #    file_type.pop()
 
             elif sortby == 'Sort by Type':
                 for file in os.listdir(get_path('src')):
                     sc = SortCriteria()
-                    is_file_in_dst_dir = os.path.isfile(get_path('dst') + "/" + file)
+                    is_file_in_dst_dir = os.path.isfile(
+                        get_path('dst') + "/" + file)
                     get_subdir()
+
                     for value in file_type:
                         if file.endswith(value):
 
                             if (is_file_in_dst_dir is False or overwrite) and get_subdir() is False:
                                 result = None
                                 if operation == "Copy":
-                                    result = shutil.copy(get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
+                                    result = shutil.copy(
+                                        get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
                                 else:
-                                    result = shutil.move(get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
+                                    result = shutil.move(
+                                        get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
+
                             elif (is_file_in_dst_dir is False or overwrite) and get_subdir() is True:
                                 result = None
                                 if operation == 'Copy':
-                                    result = shutil.copy(get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
+                                    result = shutil.copy(
+                                        get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
                                 else:
-                                    result = shutil.move(get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
+                                    result = shutil.move(
+                                        get_path('src') + "/" + file, sc.sortbytype(value) + "/" + file)
                             else:
                                 pass
 
@@ -200,9 +250,14 @@ def get_subdir():
     elif os.path.exists(str(destination) + '/' + 'Text Files'):
         return True
 
+    elif os.path.exists(str(destination) + '/' + 'Videos'):
+        return True
+
+    elif os.path.exists(str(destination) + '/' + 'Audio'):
+        return True
+
     else:
         return False
-
 
 
 class SortCriteria():
@@ -212,7 +267,7 @@ class SortCriteria():
 
         # For image files
         for type in type_list:
-            if type in image_list: #'.png' or '.jpg' or '.jpeg' or '.gif':
+            if type in image_list:  # '.png' or '.jpg' or '.jpeg' or '.gif':
                 if os.path.exists(str(destination) + '/' + 'Images'):
                     return str(os.path.join(str(destination) + '/' + 'Images'))
                 else:
@@ -229,11 +284,27 @@ class SortCriteria():
 
         # For text files
             elif type in textf_list:
-                    if os.path.exists(str(destination) + '/' + 'Text Files'):
-                        return str(os.path.join(str(destination) + '/' + 'Text Files'))
-                    else:
-                        os.mkdir(str(destination) + '/' + 'Text Files')
-                        return str(os.path.join(str(destination) + '/' + 'Text Files'))
+                if os.path.exists(str(destination) + '/' + 'Text Files'):
+                    return str(os.path.join(str(destination) + '/' + 'Text Files'))
+                else:
+                    os.mkdir(str(destination) + '/' + 'Text Files')
+                    return str(os.path.join(str(destination) + '/' + 'Text Files'))
+
+        # For video files
+            elif type in video_list:
+                if os.path.exists(str(destination) + '/' + 'Videos'):
+                    return str(os.path.join(str(destination) + '/' + 'Videos'))
+                else:
+                    os.mkdir(str(destination) + '/' + 'Videos')
+                    return str(os.path.join(str(destination) + '/' + 'Videos'))
+
+        # For audio files
+            elif type in audio_list:
+                if os.path.exists(str(destination) + '/' + 'Audio'):
+                    return str(os.path.join(str(destination) + '/' + 'Audio'))
+                else:
+                    os.mkdir(str(destination) + '/' + 'Audio')
+                    return str(os.path.join(str(destination) + '/' + 'Audio'))
 
             else:
                 sg.PopupError("File type not found!")
